22777e953603a2766943aa5029bca5276d7f4c67
Oscar Fonte <o_fonte@hotmail.com>
2024-03-26 12:09:49 +0100

    Creación lista todo

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import "./style.css";
+
+const todoForm = document.querySelector<HTMLFormElement>("form");
+const divTodoList = document.querySelector<HTMLDivElement>("#todoList");
+const inputTodo = document.querySelector<HTMLInputElement>('[name="todo"]');
+
+let todoList = [];
+
+if (todoForm != null && divTodoList != null) {
+  todoForm.onsubmit = (event) => {
+    event.preventDefault();
+    const value = inputTodo?.value;
+    let todoCheckbox = document.createElement("input");
+    todoCheckbox.setAttribute("type", "checkbox");
+    const todo = document.createElement("li");
+    todo.textContent = value || "vacio";
+    todoList.push(value);
+    todo.appendChild(todoCheckbox);
+    divTodoList.appendChild(todo);
+  };
+}
+
+//element.addEventListener("click", () => setCounter(counter + 1));
+//setCounter(0);
